45e9b16a050e971640caf0dd93d7e854541884b2
nguyenvinh01 <nguyenthanhvinh751@gmail.com>
2023-05-10 11:20:28 +0700

    commit

--- src/Store/store.jsx
+++ src/Store/store.jsx
@@ -1,7 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from "redux";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import ProductSlice from '../components/ProductSlice';
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+export const allReducers = combineReducers({
+    product: ProductSlice,
+    // nếu có reducer khác thì thêm ở đây
+});
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
 export const store = configureStore({
-    reducer: {
-        // counter: counterReducer,
-        todo: todoReducer
-    },
+    reducer: persistedReducer,
 })
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './Store/store';
+import persistStore from 'redux-persist/es/persistStore';
+import { PersistGate } from 'redux-persist/integration/react';
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>    </Provider>
   </React.StrictMode>
 );
 
